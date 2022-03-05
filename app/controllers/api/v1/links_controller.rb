df9218aaeeaf57b2eea6e863f36e8b9adbe16e31
寸田智也 <dexn@xeo.local>
2022-03-05 21:41:25 +0900

    GET /api/v1/links を修正する

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::LinksController < ApplicationController
   def index
     links = Link.order(created_at: :DESC)
-    render json: { links: LinkBlueprint.render(links) }
+    render json: { links: LinkBlueprint.render_as_json(links) }
   end
 end
